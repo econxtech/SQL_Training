9d7f79e0c18ebe46cd31e202c1b6b99b3ef59aa4
econxtech <78286118+econxtech@users.noreply.github.com>
2025-11-19 02:52:22 +0000

    Add files via upload

---
+++ Lesson1.sql
@@ -0,0 +1,65 @@
+--How to create a Database
+
+Create Database EmadeDev
+
+--Create another Database and call it EmadeTest and EmadeProd
+
+Create Database EmadeTest
+
+Create Database EmadeProd
+
+--How to remove a Database
+
+Drop Database EmadeTest
+
+--How to access a Database
+
+use EmadeDev
+
+--How to create a table that you can use inside a database
+
+--Check data in a table
+Select *
+From Customer
+
+--Show Customer in USA
+Select *
+From Customer
+Where Country ='USA'
+
+--Show CustomerID 1 3 5
+Select *
+From Customer
+Where CustomerID In (1,3,5)
+
+--Show how many Customer we have in the table
+Select Count (*) As TotalCustomer
+From Customer
+
+--Show the highest ID we have in the table
+Select MAX (CustomerID) As HighestCustomerID
+From Customer
+
+--Show the lowest ID we have in the table
+Select MIN (CustomerID) As LowestCustomerID
+From Customer
+
+
+
+
+--PRACTICE
+--Show the genders that = male
+SELECT *
+FROM Customer
+WHERE Gender = 'Male';
+
+--Show FirstName = Jerry
+SELECT *
+FROM Customer
+WHERE FirstName = 'Jerry';
+
+--Show records of people = single
+SELECT *
+FROM Customer
+WHERE MaritalStatus = 'Single';
+
---
+++ Lesson2.sql
@@ -0,0 +1,51 @@
+-- Lesson 2
+use EmadeDev
+
+-- How to remove a table from a database
+Drop Table if exists Noble
+
+--How to create a table
+Create Table Noble
+(
+[Year] int Not Null,
+[Subject] varchar (50) Null,
+[Winner] varchar (50) Null
+)
+
+--Checking data from the data
+Select * 
+From Noble
+
+--How to load data to a table
+Insert into Noble
+(
+[Year],
+[Subject],
+[Winner]
+)
+Values
+(1960,'Chemistry', 'Bob'),
+(1961,'Physics', 'Jude'),
+(1962,'Geography', 'Jason'),
+(1963,'Economics', 'Stella'),
+(1964,'Mathematics', 'Joe'),
+(1965,'English', 'Diana'),
+(1966,'Biology', 'Trump'),
+(1967,'Medicine', 'Biden'),
+(1968,'Commerce', 'Charles'),
+(1969,'Literature', 'Ben')
+
+Select *
+From Noble
+
+--Crud (Create, Read(Select), Update, Delete)
+
+--How to update a record in a table
+Update Noble
+Set [Subject] ='Law'
+Where Winner = 'Trump'
+
+--How to delete records in a table
+Delete 
+From Noble
+Where Winner = 'Trump'
